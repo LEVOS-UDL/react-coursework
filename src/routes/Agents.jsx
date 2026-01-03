01d6d6ecc7d87466dd0d302798434035f9e8e65d
LEVOS-UDL <kuzmanov427@gmail.com>
2026-01-03 20:15:44 +0000

    almost finished the agents page/remade

--- src/routes/Agents.jsx
+++ src/routes/Agents.jsx
@@ -1,16 +1,61 @@
 import React from 'react'
 import './Agents.css'
-import Left from '../assets/LEFT.png'
-import Right from '../assets/RIGHT.png'
+import Agent1 from '../assets/AGENT1.png'
+import Agent2 from '../assets/AGENT2.jpg'
+import Agent3 from '../assets/AGENT3.jpg'
+import Agent4 from '../assets/AGENT4.jpg'
+import Agent5 from '../assets/AGENT5.jpg'
 
 const Agents = () => {
   return (
     <div className='Agents'>
-      <img src={Left} alt='' className='LArrow-btn' />
-      <img src={Right} alt='' className='RArrow-btn' />
-      <div className='slider'></div>
+      <div className='Agent-Card'>
+        <img src={Agent1} alt='' className='Agent1' />
+        <div className='container'>
+          <h2>Debra Rivas</h2>
+          <p>sdasdasdasdasdas</p>
+        </div>
+      </div>
+
+      <div className='Agent-Card'>
+        <img src={Agent2} alt='' className='Agent2' />
+        <div className='container'>
+          <h2>Marcos Hoover</h2>
+          <p>sdasdasdasdasdas</p>
+        </div>
+      </div>
+
+      <div className='Agent-Card'>
+        <img src={Agent3} alt='' className='Agent3' />
+        <div className='container'>
+          <h2>Jane Wilkinson</h2>
+          <p>sdasdasdasdasdas</p>
+        </div>
+      </div>
+
+      <div className='Agent-Card'>
+        <img src={Agent4} alt='' className='Agent4' />
+        <div className='container'>
+          <h2>Trey Fischer</h2>
+          <p>sdasdasdasdasdas</p>
+        </div>
+      </div>
+
+      <div className='Agent-Card'>
+        <img src={Agent5} alt='' className='Agent5' />
+        <div className='container'>
+          <h2>Amalia Orr</h2>
+          <p>sdasdasdasdasdas</p>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Agents
+
+
+
+
+
+
