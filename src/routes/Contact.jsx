66f392dd0715f61b0caea623f71da89f5af65f58
LEVOS-UDL <kuzmanov427@gmail.com>
2026-01-03 23:54:56 +0000

    Fixed some things and added some things

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -3,9 +3,16 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>HUUUUHHHH??????</h1>
-    </div>
+    <section className='contact'>
+      <form>
+        <h2>Contact Us!</h2>
+        <div className='input'>
+          <label>Name</label>
+          <input type='text' className='field' placeholder='Enter your name' required />
+        </div>
+        
+      </form>
+    </section>
   )
 }
 
