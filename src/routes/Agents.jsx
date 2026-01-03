bdf6e4e345b994716fc51cb04214ac39388b6c84
LEVOS-UDL <kuzmanov427@gmail.com>
2026-01-03 00:58:46 +0000

    added arrows for agents

--- src/routes/Agents.jsx
+++ src/routes/Agents.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Agents.css'
+import Left from '../assets/LEFT.png'
+import Right from '../assets/RIGHT.png'
 
 const Agents = () => {
   return (
     <div className='Agents'>
-      <img src={"s"} alt='' className='\' />
-      <img src={"s"} alt='' className='\' />
+      <img src={Left} alt='' className='LArrow-btn' />
+      <img src={Right} alt='' className='RArrow-btn' />
       <div className='slider'></div>
     </div>
   )
